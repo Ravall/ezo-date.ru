5b762515782cf5a2c2ff8b90d086acbfd9bfa7de
Валерий Семенов <ravall@MacBook-Pro-Valery.local>
2016-05-24 17:13:53 +0300

    fix redirect

--- serv/biorythms/urls.py
+++ serv/biorythms/urls.py
@@ -4,13 +4,19 @@ from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from datetime import date
 from biorythms.views import IndexView, BioFormView, BioResult, AjaxGetData
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='bio_home'),
     url(r'^form$', BioFormView.as_view(), name='bio_form'),
     url(r'^result$', BioResult.as_view(), name='bio_result'),
     url(r'^ajax/get_bio_data$', AjaxGetData.as_view(), name='ajax_get_data'),
-    )
+
+    url(r'^\d{4}-\d{2}-\d{2}/\d{4}-\d{2}-\d{2}$', RedirectView.as_view(pattern_name='bio_result', permanent=True)),
+    url(r'^\d{4}-\d{2}-\d{2}$', RedirectView.as_view(pattern_name='bio_result', permanent=True))
+
+)
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- serv/solar/urls.py
+++ serv/solar/urls.py
@@ -3,6 +3,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from solar.views import IndexView, Days12View, DayView, FormView, OpenView
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = patterns('',
     url(r'^$', IndexView.as_view(), name='solar_home'),
@@ -11,6 +13,8 @@ urlpatterns = patterns('',
     url(r'^12days$', Days12View.as_view(), name='solar_12'),
     url(r'^day/(?P<nday>\d+)$', DayView.as_view(), name='solar_day'),
     url(r'^form$', FormView.as_view(), name='solar_form'),
+
+    url(r'^open/\d+/\d{12}/\d{4}$',  RedirectView.as_view(pattern_name='solar_open', permanent=True)),
 
 )
 
