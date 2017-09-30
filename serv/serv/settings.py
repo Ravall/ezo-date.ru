24fadbdf2a5aabc6d0eefd85e00ea4ff41baa2c3
ravall <ravall@MacBook-Air-Anna.local>
2017-09-30 14:04:09 +0300

    email settings

--- serv/serv/settings.py
+++ serv/serv/settings.py
@@ -65,6 +65,13 @@ if DEBUG:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_PORT = 587
+    EMAIL_HOST_USER = 'valery.ravall@gmail.com'
+    EMAIL_HOST_PASSWORD = ''
+    EMAIL_USE_TLS = True
+    SERVER_EMAIL = 'admin@ezo-date.ru'
+    DEFAULT_FROM_EMAIL = 'admin@ezo-date.ru'
 
 
 
